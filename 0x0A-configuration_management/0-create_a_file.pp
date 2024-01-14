4e249cc1a717347bb2ca1dd4605249c295884deb
Salmane Ben Messaoud <salmanesbm@gmail.com>
2024-01-14 10:18:56 -0800

    eee

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-#create a file in tmp
+# creates a file in /tmp
 
-file { '/tmp/School':
-	content => 'i love puppet',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
